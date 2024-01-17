5aed3b713d9068c3f87dc303e82aff50736195fc
khoanguyen-yang <ngn.phu.khoa@gmail.com>
2024-01-17 13:05:34 +0700

    chore: initial setup with wagmi+viem+redux

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+
+import { store } from './store/store'
+import { config } from './wagmiConfig.ts'
+
+import App from './App.tsx'
+import './index.css'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </Provider>
+  </React.StrictMode>
+)
